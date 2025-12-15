e1a42dd515ed640c68cf341f23e2e54b8f34c91e
Усков Тимофей Сергеевич <uskov.timofey@gmail.com>
2025-12-15 20:48:42 +0300

    ....

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,10 @@
 package org.example;
 
 
+import org.springframework.boot.SpringApplication;
+
 public class Main {
     public static void main(String[] args) {
-
-        }
+        SpringApplication.run(Main.class, args);
     }
+}
---
+++ src/main/java/org/example/model/WorkType.java
@@ -0,0 +1,7 @@
+package org.example.model;
+
+public enum WorkType {
+    PLANNED_REPAIR,
+    EMERGENCY_REPAIR,
+    TECHNICAL_SERVICE
+}
